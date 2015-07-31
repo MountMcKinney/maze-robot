b3f378923e8d928373d5faeb734932add1a81bdd
John <mountmckiki@gmail.com>
2015-07-31 16:13:05 -0400

    Starting out
    Creating the objects

---
+++ js/mazespace.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function MazeSpace() {
+  this.north = false;
+  this.east = false;
+  this.south = false;
+  this.west = false;
+
+}
+
+MazeSpace.prototype.setWall = function (direction) {
+  this[direction] = true;
+};
